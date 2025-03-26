1a41f9c268f4e6a57ff1111425e094e613aafe82
Luiz Gustavo A. Siewerdt <105997677+luiz-siewerdt@users.noreply.github.com>
2025-03-26 19:11:58 +0000

    Desafio Iniciante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAM_TAB 10
+
+typedef enum { H = 0, V = 1 } NavioPosicao;
+
+typedef struct {
+  NavioPosicao navioPosicao;
+  int tamanhoNavio;
+  int x, y;
+} Navio;
+
+typedef struct {
+  int tabuleiro[10][10];
+  int quantidadeNavios;
+  Navio **navios;
+} Tabuleiro;
+
+int verificarPosicao(int x, int y, int tabuleiro[TAM_TAB][TAM_TAB],
+                     NavioPosicao posicao) {
+  if (tabuleiro[y][x] == 3)
+    return 1;
+  if (posicao == 0) {
+    if (tabuleiro[y][x - 1] == 3)
+      return 1;
+    if (tabuleiro[y][x + 1] == 3)
+      return 1;
+  } else {
+    if (tabuleiro[y - 1][x] == 3)
+      return 1;
+    if (tabuleiro[y + 1][x] == 3)
+      return 1;
+  }
+
+  return 0;
+}
+
+Navio novoNavio(NavioPosicao posicao, int x, int y,
+                int tabuleiro[TAM_TAB][TAM_TAB]) {
+  if (x > TAM_TAB || x < 0 || y < 0 || y > TAM_TAB) {
+    printf("barco fora dos limites do tabuleiro.\n");
+    exit(1);
+  }
+
+  if (posicao == 0 && (x == 0 || x == 10)) {
+    printf("barco fora dos limites do tabuleiro.\n");
+    exit(1);
+  }
+
+  if (posicao == 1 && (y == 0 || y == 10)) {
+    printf("barco fora dos limites do tabuleiro.\n");
+    exit(1);
+  }
+
+  if (verificarPosicao(x, y, tabuleiro, posicao)) {
+    printf("Já existe um barco em tal local.\n");
+    exit(1);
+  }
+
+  Navio navio = {posicao, 3, x, y};
+  if (posicao == 0) {
+    tabuleiro[y][x - 1] = navio.tamanhoNavio;
+    tabuleiro[y][x] = navio.tamanhoNavio;
+    tabuleiro[y][x + 1] = navio.tamanhoNavio;
+  } else {
+    tabuleiro[y - 1][x] = navio.tamanhoNavio;
+    tabuleiro[y][x] = navio.tamanhoNavio;
+    tabuleiro[y + 1][x] = navio.tamanhoNavio;
+  }
+
+  return navio;
+}
 
 int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+  int tabuleiro[TAM_TAB][TAM_TAB] = {
+      {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+  };
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+  Navio navio = novoNavio(H, 2, 0, tabuleiro);
+  Navio navio2 = novoNavio(V, 9, 8, tabuleiro);
+  Navio navio3 = novoNavio(H, 5, 0, tabuleiro);
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+  for (int i = 0; i < TAM_TAB; i++) {
+    for (int j = 0; j < TAM_TAB; ++j) {
+      printf("%i ", tabuleiro[i][j]);
+    }
+    printf("\n");
+  }
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
-    
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
-
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
-    return 0;
+  return 0;
 }
